36009b02501e0e7f9ada759b1e8d5967c79596d2
Jesse Chan <jc@linux.com>
2021-01-17 20:37:40 +0800

    Fix URL of package

--- repo.js
+++ repo.js
@@ -1,4 +1,4 @@
 exports.name = 'Debian Repository'
 exports.description = 'Debian repository'
 
-exports.packages = ['https://github.com/jesec/rtorrent/releases/latest/download/rtorrent-amd64.deb']
+exports.packages = ['https://github.com/jesec/rtorrent.deb/releases/latest/download/rtorrent-amd64.deb']
