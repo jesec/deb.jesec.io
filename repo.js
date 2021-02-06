c8e8c01b4f9878fc1bbb87fe8e8a330d4d1038b2
Jesse Chan <jc@linux.com>
2021-02-06 21:56:30 +0800

    Add flood package

--- repo.js
+++ repo.js
@@ -4,8 +4,10 @@ exports.description = 'Debian repository'
 exports.packages = {
 	amd64: [
 		'https://nightly.link/jesec/rtorrent/workflows/publish-rolling/master/rtorrent-deb-amd64.zip',
+		'https://github.com/jesec/flood/releases/latest/download/flood-linux-x64.deb',
 	],
 	arm64: [
 		'https://nightly.link/jesec/rtorrent/workflows/publish-rolling/master/rtorrent-deb-arm64.zip',
+		'https://github.com/jesec/flood/releases/latest/download/flood-linux-arm64.deb',
 	],
 }
