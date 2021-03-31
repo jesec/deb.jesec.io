840c96f6c7d31f8a53efe0201de632157ec8b8d3
Jesse Chan <jc@linux.com>
2021-03-31 08:36:32 +0800

    Bump Flood to 4.5.1

--- repo.js
+++ repo.js
@@ -4,10 +4,10 @@ exports.description = 'Debian repository'
 exports.packages = {
 	amd64: [
 		'https://nightly.link/jesec/rtorrent/actions/runs/626407756/rtorrent-deb-amd64.zip',
-		'https://github.com/jesec/flood/releases/download/v4.5.0/flood-linux-x64.deb',
+		'https://github.com/jesec/flood/releases/download/v4.5.1/flood-linux-x64.deb',
 	],
 	arm64: [
 		'https://nightly.link/jesec/rtorrent/actions/runs/626407756/rtorrent-deb-arm64.zip',
-		'https://github.com/jesec/flood/releases/download/v4.5.0/flood-linux-arm64.deb',
+		'https://github.com/jesec/flood/releases/download/v4.5.1/flood-linux-arm64.deb',
 	],
 }
