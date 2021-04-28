d6b8297301dc1686b04400327992882918eb0914
Jesse Chan <jc@linux.com>
2021-04-28 15:03:17 +0800

    Bump Flood to 4.5.4, rTorrent to run 791138051

--- repo.js
+++ repo.js
@@ -3,11 +3,11 @@ exports.description = 'Debian repository'
 
 exports.packages = {
 	amd64: [
-		'https://nightly.link/jesec/rtorrent/actions/runs/738329976/rtorrent-deb-amd64.zip',
-		'https://github.com/jesec/flood/releases/download/v4.5.3/flood-linux-x64.deb',
+		'https://nightly.link/jesec/rtorrent/actions/runs/791138051/rtorrent-deb-amd64.zip',
+		'https://github.com/jesec/flood/releases/download/v4.5.4/flood-linux-x64.deb',
 	],
 	arm64: [
-		'https://nightly.link/jesec/rtorrent/actions/runs/738329976/rtorrent-deb-arm64.zip',
-		'https://github.com/jesec/flood/releases/download/v4.5.3/flood-linux-arm64.deb',
+		'https://nightly.link/jesec/rtorrent/actions/runs/791138051/rtorrent-deb-arm64.zip',
+		'https://github.com/jesec/flood/releases/download/v4.5.4/flood-linux-arm64.deb',
 	],
 }
