0849482d9ab64ad24298a210b29387081fbdc109
Jesse Chan <jc@linux.com>
2021-09-14 08:31:09 +0800

    Use released rTorrent packages

--- repo.js
+++ repo.js
@@ -3,11 +3,11 @@ exports.description = 'Debian repository'
 
 exports.packages = {
 	amd64: [
-		'https://nightly.link/jesec/rtorrent/actions/runs/878467218/rtorrent-deb-amd64.zip',
+		'https://github.com/jesec/rtorrent/releases/download/v0.9.8-r14/rtorrent-linux-amd64.deb',
 		'https://github.com/jesec/flood/releases/download/v4.6.0/flood-linux-x64.deb',
 	],
 	arm64: [
-		'https://nightly.link/jesec/rtorrent/actions/runs/878467218/rtorrent-deb-arm64.zip',
+		'https://github.com/jesec/rtorrent/releases/download/v0.9.8-r14/rtorrent-linux-arm64.deb',
 		'https://github.com/jesec/flood/releases/download/v4.6.0/flood-linux-arm64.deb',
 	],
 }
