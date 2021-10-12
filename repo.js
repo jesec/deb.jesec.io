e6291582120607334af703fbd7e7aac662a9af68
Jesse Chan <jc@linux.com>
2021-10-12 13:08:39 -0700

    Bump Flood to 4.7.0, rTorrent to r15

--- repo.js
+++ repo.js
@@ -3,11 +3,11 @@ exports.description = 'Debian repository'
 
 exports.packages = {
 	amd64: [
-		'https://github.com/jesec/rtorrent/releases/download/v0.9.8-r14/rtorrent-linux-amd64.deb',
-		'https://github.com/jesec/flood/releases/download/v4.6.0/flood-linux-x64.deb',
+		'https://github.com/jesec/rtorrent/releases/download/v0.9.8-r15/rtorrent-linux-amd64.deb',
+		'https://github.com/jesec/flood/releases/download/v4.7.0/flood-linux-x64.deb',
 	],
 	arm64: [
-		'https://github.com/jesec/rtorrent/releases/download/v0.9.8-r14/rtorrent-linux-arm64.deb',
-		'https://github.com/jesec/flood/releases/download/v4.6.0/flood-linux-arm64.deb',
+		'https://github.com/jesec/rtorrent/releases/download/v0.9.8-r15/rtorrent-linux-arm64.deb',
+		'https://github.com/jesec/flood/releases/download/v4.7.0/flood-linux-arm64.deb',
 	],
 }
